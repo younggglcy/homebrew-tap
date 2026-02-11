1e136e11fe2d718df90528cb6ac1448d26357384
younggglcy <younggglcy@gmail.com>
2026-02-11 13:53:39 +0000

    Update zenbar to 0.1.3

--- Casks/zenbar.rb
+++ Casks/zenbar.rb
@@ -1,6 +1,6 @@
 cask "zenbar" do
-  version "0.1.2"
-  sha256 "9d0d1e0b2a1fc65d964ad429e2bcff8c37d8664f221e8d0631a33f18b1310686"
+  version "0.1.3"
+  sha256 "e12969b1f858e819f832d21370e5b8f1ad4512b07ce6f1fa098de3629fb95ec0"
 
   url "https://github.com/younggglcy/ZenBar/releases/download/v#{version}/ZenBar-#{version}.dmg"
   name "ZenBar"
